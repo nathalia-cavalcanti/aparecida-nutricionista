33e32d9a2cb414fdcaae110d67c32b69f3d9080a
Nathalia Cavalcanti <68089638+nathalia-cavalcanti@users.noreply.github.com>
2020-10-12 21:50:06 -0300

    Add files via upload

---
+++ js/buscar-paciente.js
@@ -0,0 +1,29 @@
+var botaoBuscar = document.querySelector("#buscar-paciente");
+
+botaoBuscar.addEventListener("click", function() {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes")
+
+    
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            erroAjax.classList.add("invisivel");
+            pacientes.forEach( function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            })
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
+        
+    });
+
+    xhr.send();
+})
